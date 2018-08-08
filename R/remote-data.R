b3e9dd9740b445c7322228e9c3e129a0c8ef9d07
Joshua Kunst <jbkunst@gmail.com>
2018-08-08 11:31:34 -0400

    Update remote-data.R

--- R/remote-data.R
+++ R/remote-data.R
@@ -26,6 +26,7 @@ con <- dbConnect(
   password = "HFW9KYZBnEYr!"
 )
 
+
 DBI::dbListTables(con) %>% 
   map(tbl, src = con) %>% 
   map(tally)
@@ -34,15 +35,23 @@ tbl(con, "hogar")
 
 geoid <- tbl(con,"idgeo") %>% 
   select(COMUNA, DC, ID_ZONA_LOC) %>% 
-  collect()
+  collect(n = Inf)
 
 escolaridad <- tbl(con,"personas") %>%
   select(COMUNA, DC, ID_ZONA_LOC, P15,P09) %>%
-  collect(n)
+  collect(n = Inf)
+
+escolaridad %>% filter(!is.na(P15))
 
 escolaridad <- right_join(escolaridad, geoid, by = c("ID_ZONA_LOC","COMUNA","DC"))
+escolaridad
 
+escolaridad_comuna <- escolaridad %>%
+  filter(COMUNA %in% c("13114"))
 
+escolaridad_comuna_dc <- escolaridad_comuna %>% 
+  group_by(DC) %>% 
+  summarise_all(mean, rm.na = TRUE)
 
 archivo_shape <- "~/../Downloads/official_chile_2017_census_boundary_shapefile-master/R13/Distritos_Censales.dbf"
 
@@ -51,7 +60,38 @@ st_layers(archivo_shape)
 dcapa <- read_sf(archivo_shape, layer = "Distritos_Censales")
 dcapa <- st_transform(dcapa, 32719)
 
+dcapa <- dcapa %>% 
+  left_join(escolaridad_comuna_dc %>% mutate(COMUNA = as.character(COMUNA)), by = c("COMUNA", "COD_DISTRI" = "DC"))
 
+ggplot() +
+  geom_sf(data = dcapa, size = .2, fill = NA, colour = "gray80")
 
+plot(dcapa %>% filter(!is.na(P15)))
 
+p <- dcapa %>% 
+  filter(!is.na(P15)) %>% 
+  ggplot() +
+  geom_sf(aes(fill = P15), colour = NA) +
+  geom_sf(data = st_centroid(dcapa %>% filter(!is.na(P15))), aes(geometry = geometry, size = COD_DISTRI)) +
+  theme_minimal()
 
+p
+
+plotly::ggplotly(p)
+
+rmapshaper::ms_simplify(input = as(dcapa, 'Spatial')) %>%
+  st_as_sf() %>% 
+  ggplot() +
+  geom_sf(data = dcapa, aes(fill = P15), colour = NA) +
+  theme_bw()
+
+# otro --------------------------------------------------------------------
+archivo_shape <- "~/../Downloads/official_chile_2017_census_boundary_shapefile-master/R13/Comuna.shp"
+
+st_layers(archivo_shape)
+
+dcapa <- read_sf(archivo_shape, layer = "Comuna")
+dcapa <- st_transform(dcapa, 32719)
+
+ggplot() +
+  geom_sf(data = dcapa, size = .2, fill = NA, colour = "gray80")
